049e60d636e2a51565f59ff4a4470dc573c38fa8
Konstantin Chukhlomin <mail@chuhlomin.com>
2019-05-02 22:00:29 -0400

    Removed GOPATH, fixed JSON parsing

--- cmd/server/busdata.go
+++ cmd/server/busdata.go
@@ -16,11 +16,11 @@ type busVehicleDataMessage struct {
 	Route                string `json:"route"`                // 1
 	RunID                string `json:"runID"`                // 21
 	TripBlock            string `json:"tripBlock"`            // 001HL064
-	PatternID            string `json:"patternID`             // 264
+	PatternID            string `json:"patternID"`            // 264
 	Destination          string `json:"destination"`          // 1 NEWARK-IVY HILL VIA RIVER TERM
 	Longitude            string `json:"longitude"`            // -74.24513778686523
 	Latitude             string `json:"latitude"`             // 40.73779029846192
-	GPSTimestmp          string `json"GPStimestmp"`           // 25-Apr-2019 12:15:12 AM
+	GPSTimestmp          string `json:"GPStimestmp"`          // 25-Apr-2019 12:15:12 AM
 	LastModified         string `json:"lastModified"`         // 25-Apr-2019 12:16:10 AM
 	AsInternalTripNumber string `json:"asInternalTripNumber"` // 13734490
 	// Timepoints           []BusVehicleDataRowTimepoint `json:"timepoints"`
